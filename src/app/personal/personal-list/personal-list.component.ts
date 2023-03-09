265fc9d439626e2b94df15cdb0ba980f0290e115
miguelpgz <miguelstrock@gmail.com>
2023-03-09 13:07:34 +0100

    format code

--- src/app/personal/personal-list/personal-list.component.ts
+++ src/app/personal/personal-list/personal-list.component.ts
@@ -1,17 +1,15 @@
-import { Component, OnInit,ViewChild,ViewChildren,QueryList } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewChildren, QueryList } from '@angular/core';
 import { Table } from 'primeng/table';
 import { ConfirmationService } from 'primeng/api';
-import { DialogService, DynamicDialogConfig, DynamicDialogRef} from 'primeng/dynamicdialog';
+import { DialogService, DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { Province } from 'src/app/core/models/Province';
 import { ProvinceService } from 'src/app/core/services/province.service';
 import { CenterService } from 'src/app/core/services/center.service';
-import { SnackbarService } from 'src/app/core/services/snackbar.service';
 import { RoleService } from 'src/app/core/services/role.service';
 import { Person } from '../models/Person';
 import { PersonService } from '../services/person.service';
 import { Center } from 'src/app/core/models/Center';
 import { Person_role } from 'src/app/core/models/Person_role';
-
 import * as XLSX from 'xlsx';
 import * as FileSaver from 'file-saver';
 import { Dropdown } from 'primeng/dropdown';
@@ -38,8 +36,6 @@ export class PersonalListComponent implements OnInit {
   showDropdownFilters: boolean;
   defaultFilters: any;
 
-  cols: any[];
-
   constructor(
     private ref: DynamicDialogRef,
     private provinceService: ProvinceService,
@@ -55,12 +51,12 @@ export class PersonalListComponent implements OnInit {
     this.getAllRoles();
 
     this.defaultFilters = {
-      'active': {
-        'value': '1'
+      active: {
+        value: '1',
       },
-      'department': {
-        'value': 'CCSw'
-      }
+      department: {
+        value: 'CCSw',
+      },
     };
   }
   getAllProvinces() {
@@ -91,8 +87,6 @@ export class PersonalListComponent implements OnInit {
       },
     });
   }
-  
-  
 
   onClose(): void {
     this.ref.onClose.subscribe((results: any) => {
