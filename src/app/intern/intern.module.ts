a6effd915b86ec2db0aa0d688e11f601a9fdf511
Nikolai <the-inic@hotmail.com>
2023-03-14 15:13:34 +0100

    actualizado modulo Intern

--- src/app/intern/intern.module.ts
+++ src/app/intern/intern.module.ts
@@ -1,11 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
+import { DynamicDialogModule } from 'primeng/dynamicdialog';
 import { TableModule } from 'primeng/table';
+import { TooltipModule } from "primeng/tooltip";
+import { InternButtonsComponent } from './intern-buttons/intern-buttons.component';
 import { InternListComponent } from './intern-list/intern-list.component';
-import {DropdownModule} from 'primeng/dropdown';
+import { ConfirmDialogModule } from "primeng/confirmdialog";
+import {InputTextareaModule} from 'primeng/inputtextarea';
+import { FormsModule } from '@angular/forms';
+
+
 @NgModule({
-  declarations: [InternListComponent],
-  imports: [CommonModule, ButtonModule, TableModule,DropdownModule],
+  declarations: [InternListComponent, InternButtonsComponent],
+  imports: [
+    CommonModule,
+    ButtonModule,
+    TableModule,
+    DropdownModule,
+    CalendarModule,
+    DynamicDialogModule,
+    TooltipModule,
+    ConfirmDialogModule,
+    InputTextareaModule,
+    FormsModule
+  ],
 })
 export class InternModule {}
