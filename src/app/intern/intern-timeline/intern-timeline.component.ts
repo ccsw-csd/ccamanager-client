40cb5fed03a947e3447f079fb4fc03d46f9c13ce
Nikolai <the-inic@hotmail.com>
2023-04-27 14:21:53 +0200

    Añadida linea y cambiado grafico

--- src/app/intern/intern-timeline/intern-timeline.component.ts
+++ src/app/intern/intern-timeline/intern-timeline.component.ts
@@ -1,95 +1,162 @@
 import { Component, OnInit } from '@angular/core';
-import { InternService } from '../services/intern.service';
-import { TimeLine } from '../models/TimeLine';
-import { DynamicDialogRef } from 'primeng/dynamicdialog';
+import { Chart } from 'chart.js';
+import 'chartjs-adapter-date-fns';
+import annotationPlugin from 'chartjs-plugin-annotation';
+//import zoomPlugin from 'chartjs-plugin-zoom';
 import { PrimeNGConfig } from 'primeng/api';
+import { DynamicDialogRef } from 'primeng/dynamicdialog';
 import { TranslateService } from 'src/app/core/services/translate.service';
+import { TimeLine } from '../models/TimeLine';
+import { InternService } from '../services/intern.service';
 @Component({
   selector: 'app-intern-timeline',
   templateUrl: './intern-timeline.component.html',
-  styleUrls: ['./intern-timeline.component.scss']
+  styleUrls: ['./intern-timeline.component.scss'],
 })
 export class InternTimelineComponent implements OnInit {
-
-  timeLines:TimeLine[];
+  timeLines: TimeLine[];
   currentDate = new Date();
-  sixMonthsAgo :Date ; 
-  sixMonthsAfter :Date;
-  startDateFilter :Date;
-  endDateFilter:Date;
-  horizontalOptions:any;
-  basicData:any;
-  arrayLabels:string[];
+  sixMonthsAgo: Date;
+  sixMonthsAfter: Date;
+  config: any;
+  basicData: any;
+  arrayLabels: string[];
   constructor(
     private ref: DynamicDialogRef,
-    private internService:InternService,
-    private primengConfig:PrimeNGConfig,
-    private translateService:TranslateService
-  ){}
+    private internService: InternService,
+    private primengConfig: PrimeNGConfig,
+    private translateService: TranslateService
+  ) {}
 
   ngOnInit(): void {
+    Chart.register(annotationPlugin);
     this.primengConfig.setTranslation(this.translateService.getSpanish());
-    this.sixMonthsAgo= new Date(this.currentDate.getFullYear(), this.currentDate.getMonth() - 6, this.currentDate.getDate());
-    this.sixMonthsAfter =  new Date(this.currentDate.getFullYear(), this.currentDate.getMonth() + 6, this.currentDate.getDate());
+    this.sixMonthsAgo = new Date(
+      this.currentDate.getFullYear(),
+      this.currentDate.getMonth() - 6,
+      this.currentDate.getDate()
+    );
+    this.sixMonthsAfter = new Date(
+      this.currentDate.getFullYear(),
+      this.currentDate.getMonth() + 6,
+      this.currentDate.getDate()
+    );
     this.updateChar();
   }
-  updateChar(){
-    this.internService.findTimelineByDate(this.sixMonthsAgo,this.sixMonthsAfter).subscribe({
-      next: (res: TimeLine[]) => {
-        this.timeLines = res;
-        console.log(this.timeLines);
-        this.arrayLabels = this.timeLines.map(timeLine => timeLine.x);
-        this.basicData = {
-          labels: this.arrayLabels,
-          datasets:  [
-            {
-                label: 'My First dataset',
-                backgroundColor: '#42A5F5',
-                data: [65, 59, 80, 81, 56, 55, 40]
-            },
-            {
-                label: 'My Second dataset',
-                backgroundColor: '#FFA726',
-                data: [28, 48, 40, 19, 86, 27, 90]
-            }
-        ]
-      };
-      },
-    });
-    this.horizontalOptions = {
+   updateChar() {
+    this.internService
+      .findTimelineByDate(this.sixMonthsAgo, this.sixMonthsAfter)
+      .subscribe({
+        next: (res: TimeLine[]) => {
+          this.timeLines = res;
+          console.log(this.timeLines);
+          this.basicData = {
+            labels: this.timeLines.map((timeLine) => timeLine.x),
+            datasets: [
+              {
+                backgroundColor: this.timeLines.map(
+                  (timeline) => timeline.fillColor
+                ),
+                data: this.timeLines.map((timeLine) => timeLine.y),
+                barPercentage: 0.8,
+                order: 2,
+              },
+            ],
+          };
+        },
+      });
+    this.config = {
       indexAxis: 'y',
       plugins: {
-          legend: {
-              labels: {
-                  color: '#495057'
-              }
+        annotation: {
+          annotations: [
+            {
+              type: 'line',
+              mode: 'vertical',
+              scaleID: 'x',
+              value: new Date().getTime(),
+              borderColor: '#333333',
+              borderWidth: 1,
+              label: {
+                display:true,
+                content: 'Hoy',
+                rotation: -90,
+                yAdjust: -7,
+                enabled: true,
+                position: 'start',
+                backgroundColor: '#775DD0',
+                font: {
+                  size: 12,
+                  weight: 'bold',
+                },
+              },
+            },
+          ],
+        },
+        legend: {
+          display: true,
+          labels: {
+            generateLabels: function (chart) {
+              return [
+                { text: 'Contrato', fillStyle: '#00E396' },
+                { text: 'Vacio', fillStyle: '#008FFB' },
+                { text: 'Rechazado', fillStyle: '#FF4560' },
+              ];
+            },
+          },
+        },
+        tooltip: {
+          callbacks: {
+            label: function (context) {
+              return (
+                'Fecha: ' +
+                new Date(context.raw[0]).toLocaleDateString('es-ES') +
+                ' - ' +
+                new Date(context.raw[1]).toLocaleDateString('es-ES')
+              );
+            },
+          },
+        },
+        zoom: {
+          zoom: {
+            wheel: {
+              enabled: true,
+            },
+            pinch: {
+              enabled: true
+            },
+            mode: 'xy',
           }
+        }
       },
       scales: {
-          x: {
-              ticks: {
-                  color: '#495057'
-              },
-              grid: {
-                  color: '#ebedef'
-              }
+        x: {
+          min: this.sixMonthsAgo,
+          type: 'time',
+          time: {
+            unit: 'day',
           },
-          y: {
-              ticks: {
-                  color: '#495057'
-              },
-              grid: {
-                  color: '#ebedef'
-              }
-          }
+          position: 'bottom',
+          ticks: {
+            color: '#495057',
+          },
+          grid: {
+            color: '#ebedef',
+          },
+        },
+        y: {
+          ticks: {
+            color: '#495057',
+          },
+          grid: {
+            color: '#ebedef',
+          },
+        },
       },
-      tooltip:false,
-    };    
-    
+    };
   }
 
-  closeWindow(){
+  closeWindow() {
     this.ref.close();
   }
-
 }
