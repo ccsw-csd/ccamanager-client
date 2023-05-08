0b67c4b3a975b2bac26089ec179f8f15040b2bda
Nikolai <the-inic@hotmail.com>
2023-05-08 15:07:23 +0200

    Añadido servicio

--- src/app/pyramid/services/pyramid.service.ts
+++ src/app/pyramid/services/pyramid.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import {Pyramid} from '../models/Pyramid';
+import {PyramidTeam} from '../models/PyramidTeam';
 import { CountIndexGraph } from '../models/CountIndexGraph';
 import { CountGraph } from '../models/CountGraph';
 @Injectable({
@@ -26,5 +27,9 @@ export class PyramidService {
 
   save(pyramid:Pyramid[]):Observable<Pyramid[]>{
     return this.http.post<Pyramid[]>(environment.server+"/pyramid/",pyramid);
+  }
+
+  getAllPyramidsTeams(): Observable<PyramidTeam[]> {
+    return this.http.get<PyramidTeam[]>(environment.server + "/pyramid/team");
   }
 }
